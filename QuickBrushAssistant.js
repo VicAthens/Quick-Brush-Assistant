97971987bea1b8073449328ab62dc2f6603225d7
Athens <76474640+VicAthens@users.noreply.github.com>
2023-11-09 07:44:31 +0800

    Update QuickBrushAssistant.js

--- QuickBrushAssistant.js
+++ QuickBrushAssistant.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         直播and屏幕共享 速刷助手
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  助力业务审核
 // @author       熊喵呜哇
 // @match        https://tonghang.woa.com/v2q/qq_channel_live/audit/100000
@@ -15,73 +15,71 @@
 (function() {
     'use strict';
 
-    // 配置信息
+    let f = false;
+    let t = null;
+    let startT = Date.now();
+    let endT;
+
     const info = {
         timeGap: 5000, // 刷新超过多少秒就弹出通知
-        refreshRate: 500, // 刷新频率，单位ms
-    };
+        refreshRate: 500 // 刷新频率，单位ms
+    }
 
-    let timerId;
-    let startTimestamp;
-
-    // 提交当前case逻辑
-    function submit(e) {
-        const btn = document.querySelector('.ant-btn.antd-pro-pages-audit2-index2-submitButton.ant-btn-primary');
-        if (btn) {
-            btn.click();
-        }
+    function getDisTime(startT, endT, timeGap) {
+        let disT = endT - startT;
+        return disT >= timeGap;
     }
 
     // 刷新
     function refresh() {
-        clearTimeout(timerId);
-
-        timerId = setTimeout(() => {
-            const len = document.getElementsByTagName('form').length;
+        if (f) {
+            return;
+        }
+        f = true;
+        t = setInterval(() => {
+            let len = document.getElementsByTagName('form').length;
             if (len) {
-                // notification()
+                f = false;
+                clearInterval(t);
                 return;
             }
-            document.querySelector('.ant-menu-item.ant-menu-item-selected').children[0].click();
-            startTimestamp = Date.now();
-        }, info.refreshRate);
+            document.querySelector('.ant-menu-item.ant-menu-item-selected').children[0].click()
+        }, info.refreshRate)
     }
 
-    // mousedown事件的处理函数，用于启动刷新和提交逻辑
+    // 提交当前case逻辑
+    function submit(e) {
+        if (e.button === 1) {
+            const btn = document.querySelector('.ant-btn.antd-pro-pages-audit2-index2-submitButton.ant-btn-primary');
+            if (btn) {
+                btn.click();
+            }
+        }
+    }
+
+    // 代码入口
     function start(e) {
-        if (e.button == 1) {
+        if (e.button === 1) {
             e.preventDefault();
             submit(e);
             refresh();
         }
     }
 
-    // 键盘enter事件的处理函数，用于启动刷新逻辑
     function enterClick(e) {
         if (e.keyCode === 13) {
             refresh();
         }
     }
 
-    // 右键菜单关闭刷新功能
-    document.addEventListener('mousedown', (e) => {
-        if (e.button === 2) {
-            clearTimeout(timerId);
+    document.addEventListener('mousedown', start);
+    document.addEventListener('keydown', enterClick);
+
+    document.addEventListener("mousedown", function (e) {
+        if (e.button === 2 || e.keyCode === 32) {
+            clearInterval(t);
+            f = false;
         }
     });
 
-    // 空格键关闭刷新功能
-    document.addEventListener('keydown', (e) => {
-        if (e.keyCode === 32) {
-            clearTimeout(timerId);
-        }
-    });
-
-    // 初始化脚本
-    function init() {
-        document.addEventListener('mousedown', start);
-        document.addEventListener('keydown', enterClick);
-    }
-
-    init();
 })();
